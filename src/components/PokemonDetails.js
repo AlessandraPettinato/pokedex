8cf4b52669e8c225eb171d09f14a86aa7763b18c
Alessandra Pettinato <alessandrapettinato@Alessandras-MBP.lan>
2021-06-10 22:01:26 +0200

    Display pokemon-details. Style css accordingly

--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -1,6 +1,6 @@
-import { withRouter } from "react-router-dom";
+import { withRouter, Link } from "react-router-dom";
 
-function PokemonDetails({ routeProps, pokemon, id }) {
+function PokemonDetails({ routeProps, pokemon }) {
 	const foundCard = pokemon.find(
 		(card) => card.id === +routeProps.match.params.id
 	);
@@ -8,8 +8,30 @@ function PokemonDetails({ routeProps, pokemon, id }) {
 	if (!foundCard) return null;
 
 	return (
-		<div>
-			<p>{foundCard.base_experience}</p>
+		<div className="details-container">
+			<div className="pokemon-card-details">
+				<img
+					className="details-img"
+					src={foundCard.sprites.front_default}
+					alt=""
+				/>
+				<h1 className="pokemon-name">{foundCard.name}</h1>
+				<p className="type">
+					Type - <span>{foundCard.types[0].type.name}</span>
+				</p>
+				<p className="weight">
+					Weight - <span>{foundCard.weight}</span>
+				</p>
+				<p className="height">
+					Height - <span>{foundCard.height}</span>
+				</p>
+				<p className="ability">
+					Ability - <span>{foundCard.abilities[0].ability.name}</span>
+				</p>
+			</div>
+			<Link to="/">
+				<button className="back-to">Back to Pokemon</button>
+			</Link>
 		</div>
 	);
 }
