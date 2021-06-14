051cf010bb71262dc54855ee9397aff18dd30e7b
Alessandra Pettinato <alessandrapettinato@Alessandras-MBP.lan>
2021-06-14 17:31:50 +0200

    Fix alignment issue in PokemonDetails component

--- src/components/CapturedPokemons.js
+++ src/components/CapturedPokemons.js
@@ -8,28 +8,28 @@ export default function CapturedPokemons({ capturedPokemons }) {
 				<h1 className="captured-pokemons">Sandra's pokemons</h1>
 
 				<ul className="card-list">
-					{capturedPokemons.length === 0
-						? ""
-						: capturedPokemons.map((pokemon, index) => (
-								<li className="pokemon-card" key={index}>
-									<>
-										<AiOutlineClose className="delete-pokemon" />
-									</>
-									<img
-										className="list-img"
-										src={pokemon.sprites.front_default}
-										alt=""
-									/>
-									<p
-										style={{ fontWeight: "bold", textTransform: "capitalize" }}
-									>
-										{pokemon.name}
-									</p>
-									<Link to={`/pokemon/${pokemon.id}`}>
-										<button className="show-details">Show details</button>
-									</Link>
-								</li>
-						  ))}
+					{capturedPokemons.length === 0 ? (
+						<p>No pokemons here yet :( Go catch'em all!</p>
+					) : (
+						capturedPokemons.map((pokemon, index) => (
+							<li className="pokemon-card" key={index}>
+								<>
+									<AiOutlineClose className="delete-pokemon" />
+								</>
+								<img
+									className="list-img"
+									src={pokemon.sprites.front_default}
+									alt=""
+								/>
+								<p style={{ fontWeight: "bold", textTransform: "capitalize" }}>
+									{pokemon.name}
+								</p>
+								<Link to={`/pokemon/${pokemon.id}`}>
+									<button className="show-details">Show details</button>
+								</Link>
+							</li>
+						))
+					)}
 				</ul>
 			</div>
 			<Link to="/">
--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -8,31 +8,34 @@ function PokemonDetails({ routeProps, pokemon }) {
 	if (!foundCard) return null;
 
 	return (
-		<div className="details-container">
-			<div className="pokemon-card-details">
-				<img
-					className="details-img"
-					src={foundCard.sprites.front_default}
-					alt=""
-				/>
-				<h1 className="pokemon-name">{foundCard.name}</h1>
-				<p className="type">
-					Type - <span>{foundCard.types[0].type.name}</span>
-				</p>
-				<p className="weight">
-					Weight - <span>{foundCard.weight}</span>
-				</p>
-				<p className="height">
-					Height - <span>{foundCard.height}</span>
-				</p>
-				<p className="ability">
-					Ability - <span>{foundCard.abilities[0].ability.name}</span>
-				</p>
+		<>
+			<h1 style={{ marginTop: "4rem" }}>Pokemon details</h1>
+			<div className="details-container">
+				<div className="pokemon-card-details">
+					<img
+						className="details-img"
+						src={foundCard.sprites.front_default}
+						alt=""
+					/>
+					<h1 className="pokemon-name">{foundCard.name}</h1>
+					<p className="type">
+						Type - <span>{foundCard.types[0].type.name}</span>
+					</p>
+					<p className="weight">
+						Weight - <span>{foundCard.weight}</span>
+					</p>
+					<p className="height">
+						Height - <span>{foundCard.height}</span>
+					</p>
+					<p className="ability">
+						Ability - <span>{foundCard.abilities[0].ability.name}</span>
+					</p>
+				</div>
+				<Link to="/">
+					<button className="back-to">Back to Pokemon</button>
+				</Link>
 			</div>
-			<Link to="/">
-				<button className="back-to">Back to Pokemon</button>
-			</Link>
-		</div>
+		</>
 	);
 }
 
